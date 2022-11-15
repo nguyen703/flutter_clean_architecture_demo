ffac66e9ea72aa1909eaa20710184ff656e2564f
nguyen703 <89757787+nguyen703@users.noreply.github.com>
2022-11-15 23:07:30 +0700

    feat: add failures with dartz

--- 3_advicer/lib/1_domain/usecases/advice_usecases.dart
+++ 3_advicer/lib/1_domain/usecases/advice_usecases.dart
@@ -1,11 +1,14 @@
 import 'package:advicer/1_domain/entities/advice_entity.dart';
+import 'package:advicer/1_domain/failures/failures.dart';
+import 'package:dartz/dartz.dart';
 
 class AdviceUseCases {
   /// Get data from Repository layer.
   ///
   /// Then proceed business logic here.
-  Future<AdviceEntity> getAdvice() async {
+  Future<Either<Failure, AdviceEntity>> getAdvice() async {
     await Future.delayed(const Duration(seconds: 3), () {});
-    return const AdviceEntity(id: 0, advice: "You got a new advice");
+    return right(const AdviceEntity(id: 0, advice: "You got a new advice"));
+    // return left(ServerFailure());
   }
 }
--- 3_advicer/lib/2_application/pages/advice/bloc/advice_bloc.dart
+++ 3_advicer/lib/2_application/pages/advice/bloc/advice_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:advicer/1_domain/failures/failures.dart';
 import 'package:advicer/1_domain/usecases/advice_usecases.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
@@ -14,10 +15,25 @@ class AdviceBloc extends Bloc<AdviceEvent, AdviceState> {
     // Handle BLoC events
     on<AdviceRequestedEvent>((event, emit) async {
       emit(AdviceLoadingState());
-      final advice = await adviceUseCases.getAdvice();
-      emit(AdviceLoadedState(advice: advice.advice));
+      final failureOrAdvice = await adviceUseCases.getAdvice();
+      failureOrAdvice.fold(
+          (failure) =>
+              emit(AdviceErrorState(message: _mapFailuresToMessage(failure))),
+          (advice) => emit(AdviceLoadedState(advice: advice.advice)));
     });
 
     on<AdviceEvent>((event, emit) {});
+  }
+
+  /// Filter Failure to show the corresponding msg
+  String _mapFailuresToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return "Network error...";
+      case CacheFailure:
+        return "Failed to get cache..";
+      default:
+        return "Something's wrong..";
+    }
   }
 }
